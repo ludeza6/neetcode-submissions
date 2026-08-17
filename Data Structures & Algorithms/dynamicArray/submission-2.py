142a359c62e4be4485c1158e9a7e59a740a0e37c
ludeza6 <ludeza6@gmail.com>
2026-08-17 08:34:38 -0400

    Add: dynamicArray - submission-2

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.py
@@ -0,0 +1,45 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.length = 0
+        self.arr = [None] * capacity
+
+
+    def get(self, i: int) -> int:
+        return self.arr[i]
+
+
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n
+
+
+    def pushback(self, n: int) -> None:
+        if self.length == self.capacity:
+            self.resize()
+        
+        self.arr[self.length] = n
+        self.length += 1
+
+
+    def popback(self) -> int:
+        self.length -= 1
+        value = self.arr[self.length]
+        self.arr[self.length] = None
+        return value
+
+    def resize(self) -> None:
+        self.capacity *= 2
+        new_arr = [None] * self.capacity
+
+        for i in range(self.length):
+            new_arr[i] = self.arr[i]
+        self.arr = new_arr
+
+
+    def getSize(self) -> int:
+        return self.length
+        
+    
+    def getCapacity(self) -> int:
+        return self.capacity
